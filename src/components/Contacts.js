ea28654805ee79689f198d0aa96224dcba76413e
gurdeep4git <gurdeep.singh@prmtechnologies.com>
2019-07-09 08:16:31 +0530

    form added

---
+++ src/components/Contacts.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import Contact from "./Contact";
+
+import { Consumer } from "../context";
+
+class Contacts extends Component {
+    delete = id => {
+        const { contacts } = this.state;
+        const updatedContacts = contacts.filter(contact => {
+            return contact.id !== id;
+        });
+        this.setState({ contacts: updatedContacts });
+    };
+
+    render() {
+        return (
+            <Consumer>
+                {value => {
+                    const { contacts } = value;
+                    return (
+                        <React.Fragment>
+                            {contacts.map(contact => (
+                                <Contact
+                                    key={contact.id}
+                                    contact={contact}
+                                    onDeleteHandler={this.delete.bind(
+                                        this,
+                                        contact.id
+                                    )}
+                                />
+                            ))}
+                        </React.Fragment>
+                    );
+                }}
+            </Consumer>
+        );
+    }
+}
+
+export default Contacts;
---
+++ src/context.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+
+const Context = React.createContext();
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'DELETE_CONTACT':
+            return {
+                ...state,
+                contacts: state.contacts.filter(
+                    contact => contact.id !== action.payload
+                )
+            };
+       
+        default:
+            return state;
+    }
+};
+
+export class Provider extends Component {
+    state = {
+        contacts: [
+            {
+                id: 1,
+                name: "John Doe",
+                email: "jDoe@gmail.com",
+                phone: "555-555-5554"
+            },
+            {
+                id: 2,
+                name: "John Doe 1",
+                email: "jDoe@gmail.com",
+                phone: "555-555-5554"
+            },
+            {
+                id: 3,
+                name: "John Doe 3",
+                email: "jDoe@gmail.com",
+                phone: "555-555-5554"
+            }
+        ],
+        dispatch: action => this.setState(state => reducer(state, action))
+    };
+
+    render() {
+        return (
+            <Context.Provider value={this.state}>
+                {this.props.children}
+            </Context.Provider>
+        );
+    }
+}
+
+export const Consumer = Context.Consumer;
